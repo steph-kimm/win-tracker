5879a17cbc245fb225667289011445e7e3b9af55
Stephanie <stephanie@stephaniesair3.home>
2022-04-21 16:32:50 -0400

    fixing link

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://peaceful-lake-34508.herokuapp.com/',
+  production: 'https://peaceful-lake-34508.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
